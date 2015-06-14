9fde46f7cb4181a0dbdc71c18a436727a4740444
McCay <useruser@users-MacBook.attlocal.net>
2015-06-14 12:32:09 -0500

    Whiskey VC added

---
+++ AlCoL8oR/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  AlCoL8oR
+//
+//  Created by McCay on 6/14/15.
+//  Copyright (c) 2015 Change5. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
